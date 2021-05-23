4c59c01e05d4dd8fd22315eabf8419a6279dfc26
Azamat <educationonline@me.com>
2021-05-23 22:11:12 +0600

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class Calculator {
     private static char operator;
+    private static char choice;
     private static double num1, num2, result;
 
-    // Performs addition between numbers
+    // performs addition between numbers
     private static double Addition(double num1, double num2) {
         result = num1 + num2;
         return result;
     }
 
-    // Performs subtraction between numbers
+    // performs subtraction between numbers
     private static double Substraction(double num1, double num2) {
         result = num1 - num2;
         return result;
     }
 
-    // Performs multiplication between numbers
+    // performs multiplication between numbers
     private static double Multiply(double num1, double num2) {
         result = num1 * num2;
         return result;
     }
 
-    // Performs division between numbers
+    // performs division between numbers
     private static double Division(double num1, double num2) {
         result = num1 / num2;
         return result;
@@ -31,42 +32,52 @@ public class Calculator {
     public static void main(String[] args) {
         // Creates an object of Scanner class
         Scanner input = new Scanner(System.in);
+        
+        do {
 
-        // Asks user to enter operator
-        System.out.println("Please choose an operator: +, -, *, or /");
-        operator = input.next().charAt(0);
+            // asks user to enter operator
+            System.out.println("Please choose an operator: +, -, *, or /");
+            operator = input.next().charAt(0);
 
-        // Asks user to enter numbers
-        System.out.println("Please enter first number: ");
-        num1 = input.nextDouble();
-        System.out.println("Please enter second number: ");
-        num2 = input.nextDouble();
+            // asks user to enter numbers
+            System.out.println("Please enter first number: ");
+            num1 = input.nextDouble();
+            System.out.println("Please enter second number: ");
+            num2 = input.nextDouble();
 
-        switch (operator) {
-            case '+':
-                System.out.println(
-                        "Result: " + num1 + " + " + num2 + " = " + Addition(num1, num2));
-                break;
+            switch (operator) {
+                case '+':
+                    System.out.println(
+                            "Result: " + num1 + " + " + num2 + " = " + Addition(num1, num2));
+                    break;
 
-            case '-':
-                System.out.println(
-                        "Result: " + num1 + " - " + num2 + " = " + Substraction(num1, num2));
-                break;
+                case '-':
+                    System.out.println(
+                            "Result: " + num1 + " - " + num2 + " = " + Substraction(num1,
+                                                                                    num2));
+                    break;
 
-            case '*':
-                System.out.println(
-                        "Result: " + num1 + " * " + num2 + " = " + Multiply(num1, num2));
-                break;
+                case '*':
+                    System.out.println(
+                            "Result: " + num1 + " * " + num2 + " = " + Multiply(num1, num2));
+                    break;
 
-            case '/':
-                System.out.println(
-                        "Result: " + num1 + " / " + num2 + " = " + Division(num1, num2));
-                break;
+                case '/':
+                    System.out.println(
+                            "Result: " + num1 + " / " + num2 + " = " + Division(num1, num2));
+                    break;
 
-            default:
-                System.out.println("Invalid operator! Please re-check.");
-                break;
+                default:
+                    System.out.println("Invalid operator! Please re-check.");
+                    break;
+            }
+            // get the choice from the user to add more number
+            System.out.print("Enter \"Y\" to continue or \"N\" to exit: ");
+            choice = input.next().charAt(0);
+
         }
+        while (choice == 'Y' || choice == 'y');
         input.close();
+
     }
 }
