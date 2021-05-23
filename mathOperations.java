bf214e4f695881495a2972abf453e61d542a186b
Azamat <educationonline@me.com>
2021-05-24 03:05:09 +0600

    Update mathOperations.java

--- mathOperations.java
+++ mathOperations.java
@@ -1,28 +1,28 @@
 public class mathOperations {
     private static char operator;
     private static char choice;
-    private static long num1, num2, result;
+    private static double num1, num2, result;
 
     // performs addition between numbers
-    public static long addition(long num1, long num2) {
+    public static double addition(double num1, double num2) {
         result = num1 + num2;
         return result;
     }
 
     // performs subtraction between numbers
-    public static long subtraction(long num1, long num2) {
+    public static double subtraction(double num1, double num2) {
         result = num1 - num2;
         return result;
     }
 
     // performs multiplication between numbers
-    public static long multiply(long num1, long num2) {
+    public static double multiply(double num1, double num2) {
         result = num1 * num2;
         return result;
     }
 
     // performs division between numbers
-    public static long division(long num1, long num2) {
+    public static double division(double num1, double num2) {
         if (num2 != 0)
             result = num1 / num2;
         return result;
