bba45f74f43b0e33b1dd93617f48770202fad307
Azamat <educationonline@me.com>
2021-05-23 23:57:33 +0600

    Create mathOperations.java

---
+++ mathOperations.java
@@ -0,0 +1,33 @@
+public class mathOperations {
+    private static char operator;
+    private static char choice;
+    private static double num1, num2, result;
+
+    // performs addition between numbers
+    public static double addition(double num1, double num2) {
+        result = num1 + num2;
+        return result;
+    }
+
+    // performs subtraction between numbers
+    public static double substraction(double num1, double num2) {
+        result = num1 - num2;
+        return result;
+    }
+
+    // performs multiplication between numbers
+    public static double multiply(double num1, double num2) {
+        result = num1 * num2;
+        return result;
+    }
+
+    // performs division between numbers
+    public static double division(double num1, double num2) {
+        result = num1 / num2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
